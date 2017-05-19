46d6e365e5707b621e73ceeacd1b189099c6986f
Gili Yankovitch <creatory@gmail.com>
2017-05-19 19:20:28 +0300

    Changing design for multiple deck instances

--- src/models/deck.model.ts
+++ src/models/deck.model.ts
@@ -5,28 +5,45 @@ import { TreasureCard } from './treasure-card.model';
 import { DeadWizardCard } from './dead-wizard-card.model';
 import { AbraSourceSpell, KadabraDeliverySpell } from './spells';
 
-export abstract class Deck {
-    protected cards: Array<Card>;
-    protected static deck: Deck;
+export class Decks {
+    public SpellsDeck: SpellsDeck = new SpellsDeck();
+    public TreasureDeck: TreasureDeck = new TreasureDeck();
+    public DeadWizardDeck: DeadWizardDeck = new DeadWizardDeck();
+}
 
-    public drawCard(): Card
-    {
+abstract class Deck {
+    protected cards: Array<Card>;
+    protected discarded: Array<Card>;
+
+    public draw(): Card {
         return this.cards.pop();
     }
 
-    public shuffle()
-    {
+    public reset()  {
+        this.discarded.forEach(card => {
+            this.cards.push(card);
+        });
+
+        this.discarded = new Array<Card>();
+
+        this.shuffle();
+    }
+
+    public discard(card: Card) {
+        this.discarded.push(card);
+    }
+
+    public shuffle() {
         for (let i: number = 0; i < this.cards.length; i++)
         {
             let c: Card;
-            let r: number = Math.floor(Math.random() * (this.cards.length - i)) + i;
+            let r: number = Math.floor(Math.random() * (this.cards.length - i)) + i - 1;
 
             /* Shuffle */
             c = this.cards[r];
             this.cards[r] = this.cards[i];
             this.cards[i] = c;
         }
-        
     }
     
     public putCard(card: Card) {
@@ -44,64 +61,26 @@ export abstract class Deck {
     }
 }
 
-export class SpellsDeck extends Deck {
-    public static deck: SpellsDeck = new SpellsDeck();
-
-    private constructor() {
+class SpellsDeck extends Deck {
+    public constructor() {
         super();
         this.cards = new Array<SpellCard>();
         this.initializeDeck("SpellCard");
     }
 }
 
-export class TreasureDeck extends Deck {
-    public static deck: TreasureDeck = new TreasureDeck();
-
-    private constructor() {
+class TreasureDeck extends Deck {
+    public constructor() {
         super();
         this.cards = new Array<TreasureCard>();
         this.initializeDeck("TreasureCard");
     }
 }
 
-export class DeadWizardDeck extends Deck {
-    public static deck: DeadWizardDeck = new DeadWizardDeck();
-
-    private constructor() {
+class DeadWizardDeck extends Deck {
+    public constructor() {
         super();
         this.cards = new Array<DeadWizardCard>();
         this.initializeDeck("DeadWizartCard");
-    }
-}
-
-export class DiscardDeck extends Deck {
-    public static deck: DiscardDeck = new DiscardDeck();
-
-    private constructor() {
-        super();
-        this.cards = new Array<SpellCard>();
-    }
-
-    public discard(card: Card) {
-        this.cards.push(card);
-    }
-
-    public returnCards() {
-        let workArr: Array<Card> = this.cards.copyWithin(-1, -1);
-
-        workArr.forEach(card => {
-            if (card instanceof SpellCard)
-            {
-                SpellsDeck.deck.putCard(card);
-            }
-            else if (card instanceof TreasureCard)
-            {
-                TreasureDeck.deck.putCard(card);
-            }
-            else if (card instanceof DeadWizardCard)
-            {
-                DeadWizardDeck.deck.putCard(card);
-            }
-        });
     }
 }
